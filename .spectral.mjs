e63fae8b34a6381d1dae2f32af0249f6cbe22d2a
alex.riva2022@gmail.com <alex.riva2022@gmail.com>
2025-10-15 14:48:07 -0600

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI5NjI0Nw/spectral.js?branch_slug=master&token=9fd4f73f-5cdc-43d9-8184-e09203ae0a2c";
+export default { extends: ruleset };
